1a7e152c340f6de01de800dadf82e0aff49123fd
Mark Janssen <mark@praseodym.net>
2015-05-25 21:19:20 +0200

    Dienst2 Repository Originally from samltest branch in WISVCH/connect

--- src/test/java/ch/wisv/dienst2/apiclient/Dienst2ModelConfiguration.java
+++ src/test/java/ch/wisv/dienst2/apiclient/Dienst2ModelConfiguration.java
@@ -1,5 +1,6 @@
 package ch.wisv.dienst2.apiclient;
 
+import ch.wisv.dienst2.apiclient.util.Dienst2Repository;
 import ch.wisv.dienst2.apiclient.util.Dienst2RestTemplateFactoryBean;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -15,9 +16,17 @@ public class Dienst2ModelConfiguration {
     @Value("${dienst2.apitoken}")
     private String API_TOKEN;
 
+    @Value("${dienst2.baseurl}")
+    private String BASE_URL;
+
     @Bean
     public RestTemplate restTemplateFactory() {
         Dienst2RestTemplateFactoryBean f = new Dienst2RestTemplateFactoryBean(API_TOKEN);
         return f.getObject();
+    }
+
+    @Bean
+    public Dienst2Repository dienst2Repository() {
+        return new Dienst2Repository(restTemplateFactory(), BASE_URL);
     }
 }
--- src/test/java/ch/wisv/dienst2/apiclient/Dienst2ModelTests.java
+++ src/test/java/ch/wisv/dienst2/apiclient/Dienst2ModelTests.java
@@ -1,31 +1,22 @@
 package ch.wisv.dienst2.apiclient;
 
 import ch.wisv.dienst2.apiclient.model.Person;
-import ch.wisv.dienst2.apiclient.model.Results;
 import ch.wisv.dienst2.apiclient.model.Student;
+import ch.wisv.dienst2.apiclient.util.Dienst2Repository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.util.SerializationUtils;
-import org.springframework.web.client.RestTemplate;
 
 import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = Dienst2Model.class)
 public class Dienst2ModelTests {
-    @Value("${dienst2.baseurl}")
-    private String BASEURL;
-
     @Autowired
-    private RestTemplate restTemplate;
+    private Dienst2Repository dienst2Repository;
 
     @Test
     public void contextLoads() {
@@ -33,27 +24,15 @@ public class Dienst2ModelTests {
 
     @Test
     public void person() {
-        String url = BASEURL + "/ldb/api/v3/people/?ldap_username={username}";
-        ParameterizedTypeReference<Results<Person>> responseType = new ParameterizedTypeReference<Results<Person>>() {
-        };
-        ResponseEntity<Results<Person>> e = restTemplate.exchange(url, HttpMethod.GET, null, responseType, "mark");
-        assertEquals(e.getStatusCode(), HttpStatus.OK);
-
-        Results<Person> r = e.getBody();
-        assertEquals(r.getCount(), 1);
-        assertEquals(r.getResults().size(), 1);
-
-        Person p = r.getResults().get(0);
+        Person p = dienst2Repository.getPersonFromLdapUsername("mark").get();
         assertEquals(p.getFirstname(), "Mark");
         assertEquals(p.getCity(), "Delft");
 
-//        restTemplate.getForObject(BASEURL + p.getStudent(), Student.class);
         Student s = p.getStudent().get();
         assertEquals(s.getStudy(), "Informatica");
         assertEquals(s.isYearbookPermission(), true);
         assertEquals(s.getPerson(), p.getId());
 
-        testSerialization(r);
         testSerialization(p);
         testSerialization(s);
     }
